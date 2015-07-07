2d0082072d204a431e46df9b261141a3d4681006
Shankar Morwal <morwal89@gmail.com>
2015-07-07 21:15:37 +0530

    confirm order

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -246,7 +246,40 @@ angular.module('starter.controllers', [])
         }
     })
     .controller('confirmOrderCtrl', function ($scope, $stateParams){
+        $scope.selectedVendor={
+            id:'eN467lXt76'
+        };
+        debugger
+        $scope.currentUser = Parse.User.current();
+        $scope.selectedUser = {
+            id:'UcNI0gG0JV',
+            firstName:'saurabh',
+            lastName:'Sharma',
+            contactNo:'9602273529',
+            email:'saurabh.find@gmail.com',
+            address:'108,shiv nagar murlipura scheem'
+        }
+        $scope.orderTime = new Date();
+        $scope.orderDate = new Date();
+        $scope.menu = {
+            title:'veg'
+        }
+        $scope.vendor = {
+            title:'Rama Dabbawala'
+        }
 
+        $scope.placeOrder = function(){
+            debugger
+            var Order = Parse.Object.extend('Order');
+            var myOrder = new Order();
+            myOrder.set("Location", 'shiv nager');
+            myOrder.set("comment", 'NA');
+            myOrder.set("preferredDateTime", new Date());
+            myOrder.set("tiffins", 1);
+            myOrder.set("userId", '0FcvrSK2A6');
+            myOrder.set("vendorId", '0FcvrSK2A6');
+            myOrder.save();
+        }
     })
     .controller('ProfileCtrl', function ($scope, $stateParams){
         $scope.currentUser = Parse.User.current();
